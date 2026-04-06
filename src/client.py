884c995880987c5e231d119ca084f06976326725
Isaac <itl3@sfu.ca>
2026-04-06 13:35:04 -0700

    Handle empty client input to prevent terminal freeze

--- src/client.py
+++ src/client.py
@@ -24,17 +24,19 @@ while True:
         break
     cwd = cwd.decode("utf-8")
     cmd = input(cwd)
-    if len(str.encode(cmd)) > 0:
+    if cmd == "":
+        s.send(str.encode("\n"))
+    else:
         s.send(str.encode(cmd))
-        if (cmd == "quit"):
-            break
-        server_res = s.recv(1024)
-        if not server_res:
-            print("\nServer disconnected.")
-            break
-        server_res = server_res.decode("utf-8")
-        if server_res != "\n":
-            print(server_res, end="")
+    if (cmd == "quit"):
+        break
+    server_res = s.recv(1024)
+    if not server_res:
+        print("\nServer disconnected.")
+        break
+    server_res = server_res.decode("utf-8")
+    if server_res != "\n":
+        print(server_res, end="")
 
 s.close()
 print("Client socket closed.")
